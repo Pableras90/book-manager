cc700905877285a97c7d22c3fdcf3d6d81c941a7
Pablo <p.reinaldo90@gmail.com>
2024-01-09 20:14:17 +0100

    Entities and DbContext commit

---
+++ src/BookManager.Domain/AuthorEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace BookManager.Domain;
+
+public class AuthorEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string LastName{get; set; } = string.Empty;
+    public DateTime Birth { get; set; }
+    public string CountryCode { get; set; } = string.Empty;
+}
---
+++ src/BookManager.Persistence.SqlServer/BooksDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BookManager.Domain;
+
+
+namespace BookManager.Persistence.SqlServer;
+
+    public class BooksDbContext
+        : DbContext
+    {
+        public  BooksDbContext (DbContextOptions<BooksDbContext>options) //Dbcontext se instancia con unas opciones que vienen inyectadas(aqui va el conexion string)
+            : base(options)
+        {
+
+        }
+    // DbSet para la entidad AuthorEntity
+    public DbSet<AuthorEntity> Authors { get; set; }
+
+    // DbSet para la entidad BookEntity
+    public DbSet<BookEntity> Books { get; set; }
+
+
+   //protected override void OnModelCreating(ModelBuilder modelBuilder)
+   // {
+       
+   //     modelBuilder.Entity<AuthorEntity>().HasKey(a => a.Id);
+   //     modelBuilder.Entity<BookEntity>().HasKey(b => b.Id);
+     
+   // }
+
+    //Esto indica las claves primarias a EF, ¿es necesario?
+
+}
+
