1f3c1e848e3cc917ceb793f049a764b4b880fdee
Pablo <p.reinaldo90@gmail.com>
2024-01-16 17:51:53 +0100

    Db configuration + Initial Migration

--- src/BookManager.Persistence.SqlServer/BooksDbContext.cs
+++ src/BookManager.Persistence.SqlServer/BooksDbContext.cs
@@ -1,37 +1,36 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using BookManager.Domain;
-
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 namespace BookManager.Persistence.SqlServer;
 
-    public class BooksDbContext
-        : DbContext
+public class BooksDbContext
+    : DbContext
+{ 
+    public  BooksDbContext (DbContextOptions<BooksDbContext>options) //Dbcontext se instancia con unas opciones que vienen inyectadas(aqui va el conexion string)
+        : base(options)
     {
-        public  BooksDbContext (DbContextOptions<BooksDbContext>options) //Dbcontext se instancia con unas opciones que vienen inyectadas(aqui va el conexion string)
-            : base(options)
-        {
 
-        }
+    }
     // DbSet para la entidad AuthorEntity
     public DbSet<AuthorEntity> Authors { get; set; }
 
     // DbSet para la entidad BookEntity
     public DbSet<BookEntity> Books { get; set; }
 
-
-   protected override void OnModelCreating(ModelBuilder modelBuilder)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
 
         modelBuilder
-             .Entity<AuthorEntity>()
-             .HasKey(a => a.Id);//Esto indica las claves primarias a EF
+                .Entity<AuthorEntity>()
+                .HasKey(a => a.Id);//Esto indica las claves primarias a EF
           
-       modelBuilder
+        modelBuilder
             .Entity<BookEntity>().HasKey(b => b.Id);
         modelBuilder.Entity<AuthorEntity>()
-                 .HasMany(author => author.Books)  // Un autor tiene muchos libros
-                 .WithOne(book => book.Author)     // Un libro pertenece a un autor
-                 .HasForeignKey(book => book.AuthorId);  // Clave foránea en BookEntity que apunta a AuthorEntity
+                    .HasMany(author => author.Books)  // Un autor tiene muchos libros
+                    .WithOne(book => book.Author)     // Un libro pertenece a un autor
+                    .HasForeignKey(book => book.AuthorId);  // Clave foránea en BookEntity que apunta a AuthorEntity
     }
 }
 
--- src/BookManager/Program.cs
+++ src/BookManager/Program.cs
@@ -1,6 +1,17 @@
+using BookManager.Persistence.SqlServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+builder.Services.AddDbContext<BooksDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
+
 var app = builder.Build();
-
 app.MapGet("/", () => "Hello World!");
-
 app.Run();
